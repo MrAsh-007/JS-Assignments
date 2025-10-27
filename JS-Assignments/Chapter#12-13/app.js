3f6fc39f86ecd44a5d734b48e00e1706e5a1fff8
MrAsh-007 <mrash2006@gmail.com>
2025-10-27 16:32:11 +0500

    Chapter 12-13s Q6 and Q7 remains

--- JS-Assignments/Chapter#12-13/app.js
+++ JS-Assignments/Chapter#12-13/app.js
@@ -1,3 +1,111 @@
 // Chapter#12-13
 
 
+// // Q#1
+// // Answer Code
+// document.writeln("<br><b>Q#1</b>");
+// document.writeln("<br>");
+
+// let character = prompt("Write Anything so you can get its ASCII code");
+
+// if(character.charCodeAt(0)  >= 65 && character.charCodeAt(0) <= 90){
+//     document.writeln("It is a Capital Alphabet");
+// }else if (character.charCodeAt(0) >= 97 && character.charCodeAt(0) <= 123) {
+//     document.writeln("It is a Small Alphabet");
+// }
+
+
+
+// document.writeln("<br>");
+
+
+
+// // Q#2
+// // Answer Code
+// document.writeln("<br><b>Q#2</b>");
+// document.writeln("<br>");
+
+// let firstInteger = +prompt("Write First Integer");
+// let secondInteger = +prompt("Write Second Integer");
+
+// if(firstInteger > secondInteger){
+//     document.writeln("<br>","<b>",firstInteger,"</b>"," is Larger than ",secondInteger);
+// }else if(firstInteger < secondInteger){
+//     document.writeln("<br>","<b>",secondInteger,"</b>"," is Larger than ",firstInteger);
+// }else if(firstInteger == secondInteger){
+//     document.writeln("<br>Both Integers are equal ","<b>",firstInteger,"</b>","=","<b>",secondInteger,"</b>");
+// }
+
+
+
+// document.writeln("<br>");
+
+
+
+
+// // Q#3
+// // Answer Code
+// document.writeln("<br><b>Q#3</b>");
+// document.writeln("<br>");
+
+
+// let num = +prompt("Write a Number");
+
+// if(num > 0){
+//     document.writeln("<br>This is a positive number");
+// }else if(num < 0){
+//     document.writeln("<br>This is a negative number");
+// }else if(num === 0){
+//     document.writeln("<br>This number is zero");
+// }
+
+
+// document.writeln("<br>");
+
+
+
+
+// // Q#4
+// // Answer Code
+// document.writeln("<br><b>Q#4</b>");
+// document.writeln("<br>");
+
+
+// let char = prompt("Enter a single alphabet to check whether it is a vowel or not");
+
+// if(char == "A" || char == "a" || char == "E" || char == "e" || char == "I" || char == "i" || char == "O" || char == "o" || char == "U" || char == "u"){
+//     document.writeln("<br>True");
+// }else {
+//     document.writeln("<br>False");
+// }
+
+
+
+// document.writeln("<br>");
+
+
+
+// // Q#5
+// // Answer Code
+// document.writeln("<br><b>Q#5</b>");
+// document.writeln("<br>");
+
+// let storedPassword = "Hasan_Ashraf";
+// let checkPassword = prompt("Enter you Password");
+
+// if(checkPassword === storedPassword){
+//     document.writeln("<br>You Password is Correct!");
+// }else if(checkPassword == ""){
+//     document.writeln("<br>Please Enter Your Password");
+// }else{
+//     document.writeln("<br>Incorrect Password");
+// }
+
+
+// document.writeln("<br>");
+
+
+// Q#6
+// Answer Code
+document.writeln("<br><b>Q#6</b>");
+document.writeln("<br>");
