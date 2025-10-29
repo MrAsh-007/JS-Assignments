45471aa54effaff0a30e52340bc2c40021a3753e
MrAsh-007 <mrash2006@gmail.com>
2025-10-29 17:28:51 +0500

    Every Chapter is completed except last 5 Questions of last chapter Arrays

--- JS-Assignments/Chapter#12-13/app.js
+++ JS-Assignments/Chapter#12-13/app.js
@@ -1,111 +1,126 @@
 // Chapter#12-13
 
 
-// // Q#1
-// // Answer Code
-// document.writeln("<br><b>Q#1</b>");
-// document.writeln("<br>");
+// Q#1
+// Answer Code
+document.writeln("<br><b>Q#1</b>");
+document.writeln("<br>");
 
-// let character = prompt("Write Anything so you can get its ASCII code");
+let character = prompt("Write Anything so you can get its ASCII code");
 
-// if(character.charCodeAt(0)  >= 65 && character.charCodeAt(0) <= 90){
-//     document.writeln("It is a Capital Alphabet");
-// }else if (character.charCodeAt(0) >= 97 && character.charCodeAt(0) <= 123) {
-//     document.writeln("It is a Small Alphabet");
-// }
-
-
-
-// document.writeln("<br>");
+if(character.charCodeAt(0)  >= 65 && character.charCodeAt(0) <= 90){
+    document.writeln("It is a Capital Alphabet");
+}else if (character.charCodeAt(0) >= 97 && character.charCodeAt(0) <= 123) {
+    document.writeln("It is a Small Alphabet");
+}
 
 
 
-// // Q#2
-// // Answer Code
-// document.writeln("<br><b>Q#2</b>");
-// document.writeln("<br>");
-
-// let firstInteger = +prompt("Write First Integer");
-// let secondInteger = +prompt("Write Second Integer");
-
-// if(firstInteger > secondInteger){
-//     document.writeln("<br>","<b>",firstInteger,"</b>"," is Larger than ",secondInteger);
-// }else if(firstInteger < secondInteger){
-//     document.writeln("<br>","<b>",secondInteger,"</b>"," is Larger than ",firstInteger);
-// }else if(firstInteger == secondInteger){
-//     document.writeln("<br>Both Integers are equal ","<b>",firstInteger,"</b>","=","<b>",secondInteger,"</b>");
-// }
+document.writeln("<br>");
 
 
 
-// document.writeln("<br>");
+// Q#2
+// Answer Code
+document.writeln("<br><b>Q#2</b>");
+document.writeln("<br>");
+
+let firstInteger = +prompt("Write First Integer");
+let secondInteger = +prompt("Write Second Integer");
+
+if(firstInteger > secondInteger){
+    document.writeln("<br>","<b>",firstInteger,"</b>"," is Larger than ",secondInteger);
+}else if(firstInteger < secondInteger){
+    document.writeln("<br>","<b>",secondInteger,"</b>"," is Larger than ",firstInteger);
+}else if(firstInteger == secondInteger){
+    document.writeln("<br>Both Integers are equal ","<b>",firstInteger,"</b>","=","<b>",secondInteger,"</b>");
+}
 
 
 
-
-// // Q#3
-// // Answer Code
-// document.writeln("<br><b>Q#3</b>");
-// document.writeln("<br>");
-
-
-// let num = +prompt("Write a Number");
-
-// if(num > 0){
-//     document.writeln("<br>This is a positive number");
-// }else if(num < 0){
-//     document.writeln("<br>This is a negative number");
-// }else if(num === 0){
-//     document.writeln("<br>This number is zero");
-// }
-
-
-// document.writeln("<br>");
+document.writeln("<br>");
 
 
 
 
-// // Q#4
-// // Answer Code
-// document.writeln("<br><b>Q#4</b>");
-// document.writeln("<br>");
+// Q#4
+// Answer Code
+document.writeln("<br><b>Q#4</b>");
+document.writeln("<br>");
 
 
-// let char = prompt("Enter a single alphabet to check whether it is a vowel or not");
+let char = prompt("Enter a single alphabet to check whether it is a vowel or not");
 
-// if(char == "A" || char == "a" || char == "E" || char == "e" || char == "I" || char == "i" || char == "O" || char == "o" || char == "U" || char == "u"){
-//     document.writeln("<br>True");
-// }else {
-//     document.writeln("<br>False");
-// }
-
-
-
-// document.writeln("<br>");
+if(char == "A" || char == "a" || char == "E" || char == "e" || char == "I" || char == "i" || char == "O" || char == "o" || char == "U" || char == "u"){
+    document.writeln("<br>True");
+}else {
+    document.writeln("<br>False");
+}
 
 
 
-// // Q#5
-// // Answer Code
-// document.writeln("<br><b>Q#5</b>");
-// document.writeln("<br>");
-
-// let storedPassword = "Hasan_Ashraf";
-// let checkPassword = prompt("Enter you Password");
-
-// if(checkPassword === storedPassword){
-//     document.writeln("<br>You Password is Correct!");
-// }else if(checkPassword == ""){
-//     document.writeln("<br>Please Enter Your Password");
-// }else{
-//     document.writeln("<br>Incorrect Password");
-// }
+document.writeln("<br>");
 
 
-// document.writeln("<br>");
+
+// Q#5
+// Answer Code
+document.writeln("<br><b>Q#5</b>");
+document.writeln("<br>");
+
+let storedPassword = "Hasan_Ashraf";
+let checkPassword = prompt("Enter you Password");
+
+if(checkPassword === storedPassword){
+    document.writeln("<br>You Password is Correct!");
+}else if(checkPassword == ""){
+    document.writeln("<br>Please Enter Your Password");
+}else{
+    document.writeln("<br>Incorrect Password");
+}
+
+
+document.writeln("<br>");
 
 
 // Q#6
 // Answer Code
 document.writeln("<br><b>Q#6</b>");
+document.writeln("<br>");
+
+var greeting;
+var hour =13;
+
+if(hour < 18){
+    document.writeln(greeting = "Good Day");
+}else{
+    document.writeln(greeting = "Good Evening");
+}
+
+
+document.writeln("<br>");
+
+
+
+
+// Q#7
+// Answer Code
+document.writeln("<br><b>Q#7</b>");
+document.writeln("<br>");
+
+
+let time = prompt("Enter the current time");
+if (time >=0 && time < 1200){
+  document.writeln("Good Morning!");
+}
+if (time >=1200 && time < 1700){
+  document.writeln("Good afternoon!");
+}
+if (time >=1700 && time < 2100){
+  document.writeln("Good Evening!");
+}
+if (time >=2100 && time < 2400){
+  document.writeln("Good night!");
+}
+
 document.writeln("<br>");
---
+++ JS-Assignments/Chapter#14-16/app.js
@@ -0,0 +1,271 @@
+// Chapter#14-16
+
+
+
+
+// Q#1
+// Answer Code
+document.writeln("<br><b>Q#1</b>");
+document.writeln("<br>");
+
+let studentNames1 = [];
+
+
+document.writeln("<br>");
+
+
+
+
+// Q#2
+// Answer Code
+document.writeln("<br><b>Q#2</b>");
+document.writeln("<br>");
+
+let studentNames2 = new Array();
+
+
+document.writeln("<br>");
+
+
+
+
+// Q#3
+// Answer Code
+document.writeln("<br><b>Q#3</b>");
+document.writeln("<br>");
+
+let names = ["Ashas","Bayazeed"];
+
+
+document.writeln("<br>");
+
+
+
+
+
+// Q#5
+// Answer Code
+document.writeln("<br><b>Q#5</b>");
+document.writeln("<br>");
+
+let boolean = [true,false];
+
+
+document.writeln("<br>");
+
+
+
+
+// Q#6
+// Answer Code
+document.writeln("<br><b>Q#6</b>");
+document.writeln("<br>");
+
+let mix = ["Ashas",19,true];
+
+
+document.writeln("<br>");
+
+
+
+
+// Q#7
+// Answer Code
+document.writeln("<br><b>Q#7</b>");
+document.writeln("<br>");
+
+
+let qualifications = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.","PhD"];
+
+document.writeln("<br>1) ",qualifications[0]);
+document.writeln("<br>2) ",qualifications[1]);
+document.writeln("<br>3) ",qualifications[2]);
+document.writeln("<br>4) ",qualifications[3]);
+document.writeln("<br>5) ",qualifications[4]);
+document.writeln("<br>6) ",qualifications[5]);
+document.writeln("<br>7) ",qualifications[6]);
+document.writeln("<br>8) ",qualifications[7]);
+
+
+document.writeln("<br>");
+
+
+
+// Q#8
+// Answer Code
+document.writeln("<br><b>Q#8</b>");
+document.writeln("<br>");
+
+
+let studentNames = ["Michael","John","Tony"];
+let studentScore = [320,230,480]
+let totalMarks = 500;
+
+
+document.writeln("<br>Score of ",studentNames[0]," is ", studentScore[0],"."," Percentage: ", studentScore[0]/totalMarks*100,"%");
+document.writeln("<br>Score of ",studentNames[1]," is ", studentScore[1],"."," Percentage: ", studentScore[1]/totalMarks*100,"%");
+document.writeln("<br>Score of ",studentNames[2]," is ", studentScore[2],"."," Percentage: ", studentScore[2]/totalMarks*100,"%");
+
+
+
+document.writeln("<br>");
+
+
+
+
+// Q#9
+// Answer Code
+document.writeln("<br><b>Q#9</b>");
+document.writeln("<br>");
+
+
+
+
+
+let color = ["Red","Blue","Green"];
+document.writeln("<br>Colors: "+color);
+document.writeln("<br>");
+
+
+
+
+// a:
+document.writeln("<br><b>a:</b>");
+
+
+
+
+
+let colorAtBeginning = prompt("Enter a color to add to the beginning");
+color.unshift(colorAtBeginning);
+document.writeln("<br>Colors: "+color);
+
+
+
+
+
+document.writeln("<br>");
+
+
+
+
+
+// b:
+document.writeln("<br><b>b:</b>");
+
+
+
+
+let colorAtEnd = prompt("Enter a color to add to the end");
+color.push(colorAtEnd);
+document.writeln("<br>Colors: "+color);
+
+
+
+
+document.writeln("<br>");
+
+
+
+
+
+//c:
+document.writeln("<br><b>c:</b>");
+
+
+
+
+color.unshift("Orange","Purple");
+document.writeln("<br>Colors: "+color);
+
+
+
+
+
+document.writeln("<br>");
+
+
+
+
+
+//d:
+document.writeln("<br><b>d:</b>");
+
+
+
+
+color.shift();
+document.writeln("<br>Colors: "+color);
+
+
+
+
+document.writeln("<br>");
+
+
+
+
+
+//e:
+document.writeln("<br><b>e:</b>");
+
+
+
+
+color.pop();
+document.writeln("<br>Colors: "+color);
+
+
+
+
+document.writeln("<br>");
+
+
+
+
+
+
+//f:
+document.writeln("<br><b>f:</b>");
+
+
+
+
+let indexAdd = +prompt("Enter the position you wants to add color");
+let colorAdd = prompt("Enter the color you wants to add");
+color.splice(indexAdd,0,colorAdd);
+document.writeln("<br>Colors: "+color);
+
+
+
+
+
+
+document.writeln("<br>");
+
+
+
+
+
+//g:
+document.writeln("<br><b>g:</b>");
+
+
+
+
+let indexDelete = +prompt("Enter the position you wants to delete colors");
+let colorDelete = +prompt("How many colors you wants to delete");
+color.splice(indexDelete,colorDelete);
+document.writeln("<br>Colors: "+color);
+
+
+
+
+
+document.writeln("<br>");
+
+
+// Q#10
+// Answer Code
+document.writeln("<br><b>Q#10</b>");
+document.writeln("<br>")
