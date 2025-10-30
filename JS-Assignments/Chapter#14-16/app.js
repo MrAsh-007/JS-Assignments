c7b310f78c00e047c895a41272ab2ce33e598417
MrAsh-007 <mrash2006@gmail.com>
2025-10-30 12:57:26 +0500

    Working on Qestion#10

--- JS-Assignments/Chapter#14-16/app.js
+++ JS-Assignments/Chapter#14-16/app.js
@@ -3,269 +3,310 @@
 
 
 
-// Q#1
-// Answer Code
-document.writeln("<br><b>Q#1</b>");
-document.writeln("<br>");
+// // Q#1
+// // Answer Code
+// document.writeln("<br><b>Q#1</b>");
+// document.writeln("<br>");
 
-let studentNames1 = [];
+// let studentNames1 = [];
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
-// Q#2
-// Answer Code
-document.writeln("<br><b>Q#2</b>");
-document.writeln("<br>");
+// // Q#2
+// // Answer Code
+// document.writeln("<br><b>Q#2</b>");
+// document.writeln("<br>");
 
-let studentNames2 = new Array();
+// let studentNames2 = new Array();
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
-// Q#3
-// Answer Code
-document.writeln("<br><b>Q#3</b>");
-document.writeln("<br>");
+// // Q#3
+// // Answer Code
+// document.writeln("<br><b>Q#3</b>");
+// document.writeln("<br>");
 
-let names = ["Ashas","Bayazeed"];
+// let names = ["Ashas","Bayazeed"];
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
 
-// Q#5
-// Answer Code
-document.writeln("<br><b>Q#5</b>");
-document.writeln("<br>");
+// // Q#5
+// // Answer Code
+// document.writeln("<br><b>Q#5</b>");
+// document.writeln("<br>");
 
-let boolean = [true,false];
+// let boolean = [true,false];
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
-// Q#6
-// Answer Code
-document.writeln("<br><b>Q#6</b>");
-document.writeln("<br>");
+// // Q#6
+// // Answer Code
+// document.writeln("<br><b>Q#6</b>");
+// document.writeln("<br>");
 
-let mix = ["Ashas",19,true];
+// let mix = ["Ashas",19,true];
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
-// Q#7
-// Answer Code
-document.writeln("<br><b>Q#7</b>");
-document.writeln("<br>");
+// // Q#7
+// // Answer Code
+// document.writeln("<br><b>Q#7</b>");
+// document.writeln("<br>");
 
 
-let qualifications = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.","PhD"];
+// let qualifications = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.","PhD"];
 
-document.writeln("<br>1) ",qualifications[0]);
-document.writeln("<br>2) ",qualifications[1]);
-document.writeln("<br>3) ",qualifications[2]);
-document.writeln("<br>4) ",qualifications[3]);
-document.writeln("<br>5) ",qualifications[4]);
-document.writeln("<br>6) ",qualifications[5]);
-document.writeln("<br>7) ",qualifications[6]);
-document.writeln("<br>8) ",qualifications[7]);
+// document.writeln("<br>1) ",qualifications[0]);
+// document.writeln("<br>2) ",qualifications[1]);
+// document.writeln("<br>3) ",qualifications[2]);
+// document.writeln("<br>4) ",qualifications[3]);
+// document.writeln("<br>5) ",qualifications[4]);
+// document.writeln("<br>6) ",qualifications[5]);
+// document.writeln("<br>7) ",qualifications[6]);
+// document.writeln("<br>8) ",qualifications[7]);
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
-// Q#8
-// Answer Code
-document.writeln("<br><b>Q#8</b>");
-document.writeln("<br>");
+// // Q#8
+// // Answer Code
+// document.writeln("<br><b>Q#8</b>");
+// document.writeln("<br>");
 
 
-let studentNames = ["Michael","John","Tony"];
-let studentScore = [320,230,480]
-let totalMarks = 500;
+// let studentNames = ["Michael","John","Tony"];
+// let studentScore = [320,230,480]
+// let totalMarks = 500;
 
 
-document.writeln("<br>Score of ",studentNames[0]," is ", studentScore[0],"."," Percentage: ", studentScore[0]/totalMarks*100,"%");
-document.writeln("<br>Score of ",studentNames[1]," is ", studentScore[1],"."," Percentage: ", studentScore[1]/totalMarks*100,"%");
-document.writeln("<br>Score of ",studentNames[2]," is ", studentScore[2],"."," Percentage: ", studentScore[2]/totalMarks*100,"%");
+// document.writeln("<br>Score of ",studentNames[0]," is ", studentScore[0],"."," Percentage: ", studentScore[0]/totalMarks*100,"%");
+// document.writeln("<br>Score of ",studentNames[1]," is ", studentScore[1],"."," Percentage: ", studentScore[1]/totalMarks*100,"%");
+// document.writeln("<br>Score of ",studentNames[2]," is ", studentScore[2],"."," Percentage: ", studentScore[2]/totalMarks*100,"%");
 
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
-// Q#9
-// Answer Code
-document.writeln("<br><b>Q#9</b>");
-document.writeln("<br>");
+// // Q#9
+// // Answer Code
+// document.writeln("<br><b>Q#9</b>");
+// document.writeln("<br>");
 
 
 
 
 
-let color = ["Red","Blue","Green"];
-document.writeln("<br>Colors: "+color);
-document.writeln("<br>");
+// let color = ["Red","Blue","Green"];
+// document.writeln("<br>Colors: "+color);
+// document.writeln("<br>");
 
 
 
 
-// a:
-document.writeln("<br><b>a:</b>");
+// // a:
+// document.writeln("<br><b>a:</b>");
 
 
 
 
 
-let colorAtBeginning = prompt("Enter a color to add to the beginning");
-color.unshift(colorAtBeginning);
-document.writeln("<br>Colors: "+color);
+// let colorAtBeginning = prompt("Enter a color to add to the beginning");
+// color.unshift(colorAtBeginning);
+// document.writeln("<br>Colors: "+color);
 
 
 
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
 
-// b:
-document.writeln("<br><b>b:</b>");
+// // b:
+// document.writeln("<br><b>b:</b>");
 
 
 
 
-let colorAtEnd = prompt("Enter a color to add to the end");
-color.push(colorAtEnd);
-document.writeln("<br>Colors: "+color);
+// let colorAtEnd = prompt("Enter a color to add to the end");
+// color.push(colorAtEnd);
+// document.writeln("<br>Colors: "+color);
 
 
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
 
-//c:
-document.writeln("<br><b>c:</b>");
+// //c:
+// document.writeln("<br><b>c:</b>");
 
 
 
 
-color.unshift("Orange","Purple");
-document.writeln("<br>Colors: "+color);
+// color.unshift("Orange","Purple");
+// document.writeln("<br>Colors: "+color);
 
 
 
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
 
-//d:
-document.writeln("<br><b>d:</b>");
+// //d:
+// document.writeln("<br><b>d:</b>");
 
 
 
 
-color.shift();
-document.writeln("<br>Colors: "+color);
+// color.shift();
+// document.writeln("<br>Colors: "+color);
 
 
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
 
-//e:
-document.writeln("<br><b>e:</b>");
+// //e:
+// document.writeln("<br><b>e:</b>");
 
 
 
 
-color.pop();
-document.writeln("<br>Colors: "+color);
+// color.pop();
+// document.writeln("<br>Colors: "+color);
 
 
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
 
 
-//f:
-document.writeln("<br><b>f:</b>");
+// //f:
+// document.writeln("<br><b>f:</b>");
 
 
 
 
-let indexAdd = +prompt("Enter the position you wants to add color");
-let colorAdd = prompt("Enter the color you wants to add");
-color.splice(indexAdd,0,colorAdd);
-document.writeln("<br>Colors: "+color);
+// let indexAdd = +prompt("Enter the position you wants to add color");
+// let colorAdd = prompt("Enter the color you wants to add");
+// color.splice(indexAdd,0,colorAdd);
+// document.writeln("<br>Colors: "+color);
 
 
 
 
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
 
 
 
 
 
-//g:
-document.writeln("<br><b>g:</b>");
+// //g:
+// document.writeln("<br><b>g:</b>");
 
 
 
 
-let indexDelete = +prompt("Enter the position you wants to delete colors");
-let colorDelete = +prompt("How many colors you wants to delete");
-color.splice(indexDelete,colorDelete);
-document.writeln("<br>Colors: "+color);
+// let indexDelete = +prompt("Enter the position you wants to delete colors");
+// let colorDelete = +prompt("How many colors you wants to delete");
+// color.splice(indexDelete,colorDelete);
+// document.writeln("<br>Colors: "+color);
 
 
 
 
 
-document.writeln("<br>");
+// document.writeln("<br>");
+
+
+
 
 
 // Q#10
 // Answer Code
 document.writeln("<br><b>Q#10</b>");
 document.writeln("<br>")
+
+let scoreOfStudnts = [320,230,480,120];
+
+ scoreOfStudnts.sort((a,b)=)
+
+
+
+
+
+document.writeln("<br>")
+
+
+
+
+
+
+// // Q#11
+// // Answer Code
+// document.writeln("<br><b>Q#11</b>");
+// document.writeln("<br>")
+
+
+// let cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+
+// let selectedCities=[cities.slice(1,3)];
+
+// document.writeln("<br>","<br>Cities List: ","<br>",cities);
+// document.writeln("<br>","<br>Selected cities List: ","<br>",selectedCities);
+
+
+// document.writeln("<br>")
+
+
+
+// // Q#12
+// // Answer Code
+// document.writeln("<br><b>Q#12</b>");
+// document.writeln("<br>")
