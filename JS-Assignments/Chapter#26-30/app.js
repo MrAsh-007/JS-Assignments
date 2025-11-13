631773b064928a830656473993384961908494cf
MrAsh-007 <mrash2006@gmail.com>
2025-11-13 16:48:09 +0500

    Question 4 of Chapter#26-30 has completed

--- JS-Assignments/Chapter#26-30/app.js
+++ JS-Assignments/Chapter#26-30/app.js
@@ -1,33 +1,85 @@
 // Chapter#26-30
 
 
-// Q#1
+// // Q#1
+// // Answer Code
+// document.writeln("<br><b>Q#1</b>");
+// document.writeln("<br>");
+
+// let userNumber = +prompt("Enter a Number");
+
+// document.writeln(`<br>Number: ${userNumber}`);
+// document.writeln(`<br>Round off value: ${Math.round(userNumber)}`);
+// document.writeln(`<br>Floor value: ${Math.floor(userNumber)}`);
+// document.writeln(`<br>Ceil value: ${Math.ceil(userNumber)}`);
+
+
+
+
+// document.writeln("<br>");
+
+
+
+
+
+// // Q#2
+// // Answer Code
+// document.writeln("<br><b>Q#2</b>");
+// document.writeln("<br>");
+
+// let userNumber2 = +prompt("Enter a Negative Floating Point Number");
+
+// document.writeln(`<br>Number: ${userNumber2}`);
+// document.writeln(`<br>Round off value: ${Math.round(userNumber2)}`);
+// document.writeln(`<br>Floor value: ${Math.floor(userNumber2)}`);
+// document.writeln(`<br>Ceil value: ${Math.ceil(userNumber2)}`);
+
+
+
+
+
+
+// document.writeln("<br>");
+
+
+
+
+
+// // Q#3
+// // Answer Code
+// document.writeln("<br><b>Q#3</b>");
+// document.writeln("<br>");
+
+// let userNumber3 = +prompt("Enter a Number to get its Absolute Value");
+
+// document.writeln(`<br>The Absolute Value of ${userNumber3} is ${Math.abs(userNumber3)}`);
+
+
+
+
+// document.writeln("<br>");
+
+
+
+
+
+// // Q#4
+// // Answer Code
+// document.writeln("<br><b>Q#4</b>");
+// document.writeln("<br>");
+
+// let dice = Math.floor(Math.random() * 6) + 1;
+
+// document.writeln(dice);
+
+
+
+// document.writeln("<br>");
+
+
+
+
+// Q#5
 // Answer Code
-document.writeln("<br><b>Q#1</b>");
-document.writeln("<br>");
-
-let userNumber = +prompt("Enter a Number");
-document.writeln(`<br>Number: ${userNumber}`);
-document.writeln(`<br>Round off value: ${Math.round(userNumber)}`);
-document.writeln(`<br>Floor value: ${Math.floor(userNumber)}`);
-document.writeln(`<br>Ceil value: ${Math.ceil(userNumber)}`);
-
-
-
-
-document.writeln("<br>");
-
-
-
-
-
-// Q#2
-// Answer Code
-document.writeln("<br><b>Q#2</b>");
-document.writeln("<br>");
-
-
-
-
-
+document.writeln("<br><b>Q#5</b>");
 document.writeln("<br>");
