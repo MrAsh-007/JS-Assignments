db8b7d178206c15d932ee06c34df60643756f247
MrAsh-007 <mrash2006@gmail.com>
2025-11-13 10:55:34 +0500

    Working on Chapter#26-30

---
+++ JS-Assignments/Chapter#26-30/app.js
@@ -0,0 +1,33 @@
+// Chapter#26-30
+
+
+// Q#1
+// Answer Code
+document.writeln("<br><b>Q#1</b>");
+document.writeln("<br>");
+
+let userNumber = +prompt("Enter a Number");
+document.writeln(`<br>Number: ${userNumber}`);
+document.writeln(`<br>Round off value: ${Math.round(userNumber)}`);
+document.writeln(`<br>Floor value: ${Math.floor(userNumber)}`);
+document.writeln(`<br>Ceil value: ${Math.ceil(userNumber)}`);
+
+
+
+
+document.writeln("<br>");
+
+
+
+
+
+// Q#2
+// Answer Code
+document.writeln("<br><b>Q#2</b>");
+document.writeln("<br>");
+
+
+
+
+
+document.writeln("<br>");
