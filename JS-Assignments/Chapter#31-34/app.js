93ba8884ab14859cd74833f4f587675f4475e0ba
MrAsh-007 <mrash2006@gmail.com>
2025-11-19 16:07:19 +0500

    Working on Chapter#31-34

---
+++ JS-Assignments/Chapter#31-34/app.js
@@ -0,0 +1,132 @@
+// Chapter#31-34
+
+
+
+// // Q#1
+// // Answer Code
+// document.writeln(`<br><b>Q#1</b>`);
+// document.writeln(`<br>`);
+
+
+// let date1 = new Date();
+
+// document.writeln(`<br>${date1}`);
+
+
+
+
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+
+// // Q#2
+// // Answer Code
+// document.writeln(`<br><b>Q#2</b>`);
+// document.writeln(`<br>`);
+
+// let months = ["January","February", "March", "April", "May", "June", "July", "August", "September", "Octuber", "November", "December"];
+
+// let currentMonth = months[new Date().getMonth()];
+
+// document.writeln(`<br>Current month: ${currentMonth}`);
+
+
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+
+// // Q#3
+// // Answer Code
+// document.writeln(`<br><b>Q#3</b>`);
+// document.writeln(`<br>`);
+
+// let daysInAWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// let today = daysInAWeek[new Date().getDay()];
+
+// document.writeln(`<br>Today is ${today}`);
+
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+
+
+
+
+
+// // Q#4
+// // Answer Code
+// document.writeln(`<br><b>Q#4</b>`);
+// document.writeln(`<br>`);
+
+
+// daysInAWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// today = daysInAWeek[new Date().getDay()];
+
+
+// if(today == daysInAWeek[0] || today == daysInAWeek[6]){
+//     document.writeln(`<br>It's Fun day`);
+// }else{
+//     document.writeln(`<br>Today is ${today}`);
+// }
+
+
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+
+
+// // Q#5
+// // Answer Code
+// document.writeln(`<br><b>Q#5</b>`);
+// document.writeln(`<br>`);
+
+// let date5 = new Date().getDate();
+
+// if(date5 < 16){
+//     document.writeln(`<br>First fifteen days of the month`);
+// }else{
+//     document.writeln(`<br>Last days of the month`);
+// }
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+
+// Q#6
+// Answer Code
+document.writeln(`<br><b>Q#6</b>`);
+document.writeln(`<br>`);
+
+
+document.writeln(`<br>`);
