6e655cea6dd536f48dd8e59a2a33855947114e10
MrAsh-007 <mrash2006@gmail.com>
2025-11-21 17:51:16 +0500

    Question#1-5 has completed

---
+++ JS-Assignments/Chapter#35-38/app.js
@@ -0,0 +1,132 @@
+// Chapter#35-38
+
+
+
+// // Q#1
+// // Answer Code
+// document.writeln(`<br><b>Q#1</b>`);
+// document.writeln(`<br>`);
+
+// function currentDateNTime(){
+//     let currentDateNTime = new Date();
+//     return document.writeln(`<br>${currentDateNTime}`);
+// }
+// currentDateNTime();
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+// // Q#2
+// // Answer Code
+// document.writeln(`<br><b>Q#2</b>`);
+// document.writeln(`<br>`);
+
+// function userGreet(firstName, lastName){
+//     let fullName = `${firstName} ${lastName}`;
+
+//     if(firstName == null && lastName == null){
+//         return userGreet()
+//     }else{
+//         return alert(`Khush Aamdeed ${fullName}`);
+//     }
+// }
+
+// userGreet(prompt(`Enter Your First Name`), prompt(`Enter Your Last Name`));
+
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+
+// // Q#3
+// // Answer Code
+// document.writeln(`<br><b>Q#3</b>`);
+// document.writeln(`<br>`);
+
+// function userOperation(firstNum, secondNum){
+//     let sum3 = firstNum + secondNum;
+//     document.writeln(`<br>The sum of ${firstNum} and ${secondNum} is ${sum3}`)
+// }
+// userOperation(+prompt(`Enter First Number`, 0), +prompt(`Enter Second Number`, 0));
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+// // Q#4
+// // Answer Code
+// document.writeln(`<br><b>Q#4</b>`);
+// document.writeln(`<br>`);
+
+// function userCalculator(num1, num2, operation){
+//     if(operation == "+"){
+//         let sum = num1 + num2;
+//         return document.writeln(`The Sum of ${num1} and ${num2} is ${sum}`);
+//     }else if(operation == "x"){
+//         let product = num1 * num2;
+//         document.writeln(`The Product of ${num1} and ${num2} is ${product}`);
+//     }else if(operation == "-"){
+//         let difference = num1 - num2;
+//         document.writeln(`The Difference of ${num1} and ${num2} is ${difference}`);
+//     }else if(operation == "/"){
+//         let quotient = num1 / num2;
+//         document.writeln(`The Quotient of ${num1} and ${num2} is ${quotient}`);
+//     }else if(operation == "%"){
+//         let remainder = num1 % num2;
+//         document.writeln(`The Remainder of ${num1} and ${num2} is ${remainder}`);
+//     }
+// }
+// userCalculator(+prompt(`Enter first number`), +prompt(`Enter second number`), prompt(`Enter operation sign: + - x / %`));
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+// // Q#5
+// // Answer Code
+// document.writeln(`<br><b>Q#5</b>`);
+// document.writeln(`<br>`);
+
+// function square(number){
+//     let square = number**2
+//     return document.writeln(`The Square of ${number} is ${square}`);
+// }
+// square(prompt(`Enter a number to get its square`));
+
+
+
+// document.writeln(`<br>`);
+
+
+
+
+
+
+// Q#6
+// Answer Code
+document.writeln(`<br><b>Q#6</b>`);
+document.writeln(`<br>`);
+
+
+
+
+
+
+document.writeln(`<br>`);
