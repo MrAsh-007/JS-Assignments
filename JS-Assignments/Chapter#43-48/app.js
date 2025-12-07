b8ad2b5240be2029dea0efaae00f89bc19e110d5
MrAsh-007 <mrash2006@gmail.com>
2025-12-07 15:38:37 +0500

    Working on Ch#43-48 and Q#3 and 5 remains

--- JS-Assignments/Chapter#43-48/app.js
+++ JS-Assignments/Chapter#43-48/app.js
@@ -26,6 +26,33 @@
 // Q#3
 // Answer Code
 
-function deleteData() {
-    
+function mobileAlert(e){
+    alert("Thanks for purchasing phone from us");
+}
+let heading2 = document.getElementById("heading2");
+heading2.style.color = "skyblue";
+
+let container = document.getElementById("imgContainer");
+container.style.display = "flex";
+container.style.flexDirection = "row";
+container.style.justifyContent = "space-around";
+
+let images = [document.getElementById("img1"), document.getElementById("img2"), document.getElementById("img3"), document.getElementById("img4")];
+
+for(let i = 0; i < images.length; i++){
+    images[i].style.border = "2px solid skyblue"
+    images[i].style.cursor = "pointer"
+}
+
+
+
+
+
+
+
+// Q#4
+// Answer Code
+function imgConversion(e) {
+    e.src = "Images/animatedpic.jpg";
+    e.style.cursor = "pointer"
 }
